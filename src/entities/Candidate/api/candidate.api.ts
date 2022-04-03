0aca0c43b0387d6c39cd526be94c8db92149858f
nurbolatk <nurbolattk@gmail.com>
2022-04-03 13:07:35 +0600

    feat: get all candidates

--- src/entities/Candidate/api/candidate.api.ts
+++ src/entities/Candidate/api/candidate.api.ts
@@ -4,3 +4,7 @@ import { NewCandidateFields, Entity } from '../types';
 export const createEntity = async (data: NewCandidateFields, token: string) => {
   return client<Entity>('candidate', { data, token });
 };
+
+export const getAll = async () => {
+  return client<Entity[]>('candidate');
+};
--- src/pages/recruiting/index.tsx
+++ src/pages/recruiting/index.tsx
@@ -1,14 +1,44 @@
-import { Button } from '@mantine/core';
+import { Button, Table, Title } from '@mantine/core';
+import { api, Entity } from 'entities/Candidate';
 import React from 'react';
+import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 
 export function RecruitingIndexRoute() {
+  const { data: candidates } = useQuery<Entity[]>('candidates', api.getAll);
+
+  const rows = candidates?.map((candidate: Entity) => {
+    return (
+      <tr key={candidate.id}>
+        <td>{candidate.firstName}</td>
+        <td>{candidate.lastName}</td>
+      </tr>
+    );
+  });
+
   return (
     <div>
-      <Button<typeof Link> component={Link} to="/candidates/new">
-        Add candidate
-      </Button>
-      <Button variant="outline">Send application</Button>
+      <Title order={2}>Candidates</Title>
+      <div>
+        <Button<typeof Link> component={Link} to="/candidates/new">
+          Add candidate
+        </Button>
+        <Button variant="outline">Send application</Button>
+      </div>
+      <section>
+        <Table>
+          <thead>
+            <tr>
+              <th>Element position</th>
+              <th>Element name</th>
+              <th>Symbol</th>
+              <th>Atomic mass</th>
+            </tr>
+          </thead>
+          {!rows && <p>No candidates</p>}
+          {rows && <tbody>{rows}</tbody>}
+        </Table>
+      </section>
     </div>
   );
 }
