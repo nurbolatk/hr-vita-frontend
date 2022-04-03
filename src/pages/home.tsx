df3f7793652933637ff4be763581da77e3c90a60
nurbolatk <nurbolattk@gmail.com>
2022-04-03 11:00:30 +0600

    feat: swiper autoplay

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,28 +1,31 @@
 import { Anchor, Card, Title } from '@mantine/core';
 import React from 'react';
 import { DatabaseIcon } from 'shared/components/icons';
+import { Autoplay } from 'swiper';
+import 'swiper/css/autoplay';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 export function HomeRoute(): JSX.Element {
   return (
     <div>
       <Swiper
+        modules={[Autoplay]}
         spaceBetween={50}
         slidesPerView={1}
         autoplay
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}>
         <SwiperSlide>
-          <div className="h-72 w-full bg-purple-300 rounded-lg">news!</div>
+          <div className="h-72 w-full bg-purple-300 rounded-lg">news! 1</div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-72 w-full bg-purple-300 rounded-lg">news!</div>
+          <div className="h-72 w-full bg-purple-300 rounded-lg">news! 2</div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-72 w-full bg-purple-300 rounded-lg">news!</div>
+          <div className="h-72 w-full bg-purple-300 rounded-lg">news! 3</div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-72 w-full bg-purple-300 rounded-lg">news!</div>
+          <div className="h-72 w-full bg-purple-300 rounded-lg">news! 4</div>
         </SwiperSlide>
       </Swiper>
 
