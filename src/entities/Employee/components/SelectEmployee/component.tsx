78ba451312e1c05e267afc2c5cb15d9f6c0b1804
nurbolatk <nurbolattk@gmail.com>
2022-04-05 22:27:36 +0600

    fix: use remote data for SelectEmployee

--- src/entities/Employee/components/SelectEmployee/component.tsx
+++ src/entities/Employee/components/SelectEmployee/component.tsx
@@ -1,37 +1,15 @@
 import { api } from 'entities/Employee/api';
 import React, { useState } from 'react';
 import { useQuery } from 'react-query';
-import Select, { ActionMeta, OnChangeValue } from 'react-select';
+import Select, { OnChangeValue } from 'react-select';
 import type { Employee } from '../../types';
 
-const options: Employee[] = [
-  {
-    id: 1,
-    firstName: 'Aloha',
-    lastName: 'Dev',
-    email: 'curious@dev.com',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-];
-
-// const filterOptions = (candidate: { label: string; value: string; data: any }, input: string) => {
-//   if (input) {
-//     return candidate.value === customOptions[0].value;
-//   }
-//   return true;
-// };
-
 export function SelectEmployee({ onChange }: { onChange: (value: Employee | null) => void }): JSX.Element {
-  const { isLoading } = useQuery<Employee[]>('employees', api.getAll);
+  const { data, isLoading } = useQuery<Employee[]>('employees', api.getAll);
 
   const [value, setValue] = useState<Employee | null | undefined>();
 
-  const handleChange = (newValue: OnChangeValue<Employee, false>, actionMeta: ActionMeta<Employee>) => {
-    console.group('Value Changed');
-    console.log(newValue);
-    console.log(`action: ${actionMeta.action}`);
-    console.groupEnd();
+  const handleChange = (newValue: OnChangeValue<Employee, false>) => {
     setValue(newValue);
     onChange(newValue);
   };
@@ -44,7 +22,7 @@ export function SelectEmployee({ onChange }: { onChange: (value: Employee | null
       isLoading={isLoading}
       onChange={handleChange}
       getOptionLabel={(option) => `${option.firstName} ${option.lastName}`}
-      options={options}
+      options={data}
       value={value}
     />
   );
