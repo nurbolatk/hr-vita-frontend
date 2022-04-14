a88f9e72a90f43e03e4e55358ea5949acf42fcf8
nurbolatk <nurbolattk@gmail.com>
2022-04-15 01:38:17 +0600

    feat: add Breadcrumbs

--- src/entities/Candidate/components/EditCandidateForm/component.tsx
+++ src/entities/Candidate/components/EditCandidateForm/component.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-nested-ternary */
-import { Button, Card, LoadingOverlay, TextInput } from '@mantine/core';
+import { Anchor, Breadcrumbs, Button, Card, LoadingOverlay, TextInput } from '@mantine/core';
 import { useAuth } from 'app/providers';
 import { dequal } from 'dequal';
 import { api } from 'entities/Candidate';
@@ -13,6 +13,7 @@ import { useMutation, useQueryClient } from 'react-query';
 import { useIdParam } from 'shared/hooks';
 import { CandidateStatus } from 'entities/Candidate/types';
 import { parseCandidateStatusJSX } from 'entities/Candidate/helpers';
+import { Link } from 'react-router-dom';
 import type { CandidateFormFields, DefaultCandidateFields, UpdateCandidateData } from '../../types';
 
 export function EditCandidateForm({ defaultValues }: { defaultValues: DefaultCandidateFields }): JSX.Element {
@@ -84,131 +85,126 @@ export function EditCandidateForm({ defaultValues }: { defaultValues: DefaultCan
   };
 
   return (
-    <section className="mx-auto grid grid-cols-1 md:grid-cols-5 gap-4 items-start">
-      <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4 relative col-span-3">
-        <LoadingOverlay visible={mutation.isLoading} />
-        <Card withBorder shadow="md" p="lg">
-          <h3 className="mb-3 text-xl flex items-baseline justify-between">
-            Profile
-            {isChanged && (
-              <div>
-                <Button type="button" className="mr-2" variant="default" compact onClick={cancelChanges}>
-                  Cancel
-                </Button>
-                <Button type="submit" compact>
-                  Save
-                </Button>
+    <section className="mx-auto">
+      <Breadcrumbs className="mb-4">
+        {[
+          { title: 'Recruiting', href: '/recruiting' },
+          { title: `${defaultValues.form.firstName} ${defaultValues.form.lastName}`, href: `/recruiting/${id}` },
+        ].map((item) => (
+          <Anchor<typeof Link> component={Link} to={item.href} key={item.href}>
+            {item.title}
+          </Anchor>
+        ))}
+      </Breadcrumbs>
+
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4 items-start">
+        <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4 relative col-span-3">
+          <LoadingOverlay visible={mutation.isLoading || statusMutation.isLoading} />
+          <Card withBorder shadow="md" p="lg">
+            <h3 className="mb-3 text-xl flex items-baseline justify-between">
+              Profile
+              {isChanged && (
+                <div>
+                  <Button type="button" className="mr-2" variant="default" compact onClick={cancelChanges}>
+                    Cancel
+                  </Button>
+                  <Button type="submit" compact>
+                    Save
+                  </Button>
+                </div>
+              )}
+            </h3>
+            <div className="space-y-2">
+              <div className="flex items-baseline gap-2 text-sm">
+                Status: {parseCandidateStatusJSX(defaultValues.status)}
               </div>
-            )}
-          </h3>
-          <div className="space-y-2">
-            <div className="flex items-baseline gap-2 text-sm">
-              Status: {parseCandidateStatusJSX(defaultValues.status)}
-            </div>
-            <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
-              <TextInput
-                label="Имя"
-                className="font-medium block"
-                type="text"
-                // required
-                {...register(
-                  'firstName'
-                  // { required: 'Необходимо заполнить' }
-                )}
-                error={errors.firstName?.message}
-              />
-              <TextInput
-                label="Фамилия"
-                className="font-medium block"
-                type="text"
-                // required
-                {...register(
-                  'lastName'
-                  // { required: 'Необходимо заполнить' }
-                )}
-                error={errors.lastName?.message}
-              />
-            </div>
+              <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
+                <TextInput
+                  label="Имя"
+                  className="font-medium block"
+                  type="text"
+                  // required
+                  {...register(
+                    'firstName'
+                    // { required: 'Необходимо заполнить' }
+                  )}
+                  error={errors.firstName?.message}
+                />
+                <TextInput
+                  label="Фамилия"
+                  className="font-medium block"
+                  type="text"
+                  // required
+                  {...register(
+                    'lastName'
+                    // { required: 'Необходимо заполнить' }
+                  )}
+                  error={errors.lastName?.message}
+                />
+              </div>
 
-            <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
-              <TextInput
-                label="Эл. почта"
-                className="font-medium block"
-                type="email"
-                // required
-                {...register(
-                  'email'
-                  // { required: 'Необходимо заполнить' }
-                )}
-                error={errors.email?.message}
-              />
-              <TextInput
-                label="Номер телефона"
-                className="font-medium block"
-                type="tel"
-                {...register('phone')}
-                error={errors.phone?.message}
-              />
-            </div>
+              <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
+                <TextInput
+                  label="Эл. почта"
+                  className="font-medium block"
+                  type="email"
+                  // required
+                  {...register(
+                    'email'
+                    // { required: 'Необходимо заполнить' }
+                  )}
+                  error={errors.email?.message}
+                />
+                <TextInput
+                  label="Номер телефона"
+                  className="font-medium block"
+                  type="tel"
+                  {...register('phone')}
+                  error={errors.phone?.message}
+                />
+              </div>
 
-            <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
-              <TextInput
-                label="Зарплата"
-                className="mb-1 font-medium block"
-                type="number"
-                {...register('salary')}
-                error={errors.salary?.message}
-              />
-              <TextInput
-                label="Место жительства"
-                className="mb-1 font-medium block"
-                type="text"
-                {...register('location')}
-                error={errors.location?.message}
-              />
-            </div>
+              <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
+                <TextInput
+                  label="Зарплата"
+                  className="mb-1 font-medium block"
+                  type="number"
+                  {...register('salary')}
+                  error={errors.salary?.message}
+                />
+                <TextInput
+                  label="Место жительства"
+                  className="mb-1 font-medium block"
+                  type="text"
+                  {...register('location')}
+                  error={errors.location?.message}
+                />
+              </div>
 
-            <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
-              <Controller
-                control={control}
-                name="position"
-                // rules={{ required: 'Надо выбрать' }}
-                render={({ field: { value, onChange }, fieldState: { error } }) => {
-                  return (
-                    <SelectPosition setValue={setValue} value={value} onChange={onChange} error={error?.message} />
-                  );
-                }}
-              />
-              <Controller
-                control={control}
-                name="department"
-                // rules={{ required: 'Надо выбрать' }}
-                render={({ field: { value, onChange }, fieldState: { error } }) => {
-                  return (
-                    <SelectDepartment setValue={setValue} value={value} onChange={onChange} error={error?.message} />
-                  );
-                }}
-              />
+              <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
+                <Controller
+                  control={control}
+                  name="position"
+                  // rules={{ required: 'Надо выбрать' }}
+                  render={({ field: { value, onChange }, fieldState: { error } }) => {
+                    return (
+                      <SelectPosition setValue={setValue} value={value} onChange={onChange} error={error?.message} />
+                    );
+                  }}
+                />
+                <Controller
+                  control={control}
+                  name="department"
+                  // rules={{ required: 'Надо выбрать' }}
+                  render={({ field: { value, onChange }, fieldState: { error } }) => {
+                    return (
+                      <SelectDepartment setValue={setValue} value={value} onChange={onChange} error={error?.message} />
+                    );
+                  }}
+                />
+              </div>
             </div>
-          </div>
-        </Card>
-        <Card
-          withBorder
-          shadow="sm"
-          p="lg"
-          style={{
-            overflow: 'visible',
-          }}>
-          <div className="flex mb-3 items-center gap-x-4">
-            <h3 className="text-xl">Documents</h3>
-          </div>
-          <UploadFile uploaded={uploaded} setUploaded={setUploaded} />
-        </Card>
-      </form>
-
-      <div className="grid col-span-3 md:col-span-2 gap-4">
-        <InterviewsTimeline />
-        {defaultValues.status !== CandidateStatus.HIRED && defaultValues.status !== CandidateStatus.FAILED && (
+          </Card>
           <Card
             withBorder
             shadow="sm"
@@ -216,30 +212,48 @@ export function EditCandidateForm({ defaultValues }: { defaultValues: DefaultCan
             style={{
               overflow: 'visible',
             }}>
-            <h3 className="text-xl mb-3">Update candidate status</h3>
-            <div className="flex items-center gap-x-4">
-              <Button
-                variant="light"
-                onClick={() => {
-                  statusMutation.mutate({
-                    status: CandidateStatus.HIRED,
-                  });
-                }}>
-                Hire candidate
-              </Button>
-              <Button
-                variant="light"
-                color="red"
-                onClick={() => {
-                  statusMutation.mutate({
-                    status: CandidateStatus.FAILED,
-                  });
-                }}>
-                Reject candidate
-              </Button>
+            <div className="flex mb-3 items-center gap-x-4">
+              <h3 className="text-xl">Documents</h3>
             </div>
+            <UploadFile uploaded={uploaded} setUploaded={setUploaded} />
           </Card>
-        )}
+        </form>
+
+        <div className="grid col-span-3 md:col-span-2 gap-4">
+          <InterviewsTimeline />
+          {defaultValues.status !== CandidateStatus.HIRED && defaultValues.status !== CandidateStatus.FAILED && (
+            <Card
+              withBorder
+              shadow="sm"
+              p="lg"
+              style={{
+                overflow: 'visible',
+              }}>
+              <h3 className="text-xl mb-3">Update candidate status</h3>
+              <div className="flex items-center gap-x-4">
+                <Button
+                  variant="light"
+                  onClick={() => {
+                    statusMutation.mutate({
+                      status: CandidateStatus.HIRED,
+                    });
+                  }}>
+                  Hire candidate
+                </Button>
+                <Button
+                  variant="light"
+                  color="red"
+                  onClick={() => {
+                    statusMutation.mutate({
+                      status: CandidateStatus.FAILED,
+                    });
+                  }}>
+                  Reject candidate
+                </Button>
+              </div>
+            </Card>
+          )}
+        </div>
       </div>
     </section>
   );
--- src/entities/Employee/components/EditEmployeeForm/component.tsx
+++ src/entities/Employee/components/EditEmployeeForm/component.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, LoadingOverlay, Text, SegmentedControl, TextInput } from '@mantine/core';
+import { Button, Card, LoadingOverlay, Text, SegmentedControl, TextInput, Breadcrumbs, Anchor } from '@mantine/core';
 import {
   api,
   CreateEmployeeDTO,
@@ -17,6 +17,7 @@ import { useMutation, useQueryClient } from 'react-query';
 import { useIdParam } from 'shared/hooks';
 import { ApprovalsTimeline } from 'entities/Approval';
 import { dequal } from 'dequal';
+import { Link } from 'react-router-dom';
 
 export function EditEmployeeForm({ defaultValues }: { defaultValues: DefaultEmployeeFields }): JSX.Element {
   const {
@@ -52,7 +53,6 @@ export function EditEmployeeForm({ defaultValues }: { defaultValues: DefaultEmpl
   const values = getValues();
   const isFormChanged = useMemo(() => !dequal(defaultValues.form, values), [defaultValues.form, values]);
   const isChanged = isFormChanged || areDocumentsChanged;
-  console.log(isFormChanged);
 
   const cancelChanges = () => {
     reset();
@@ -75,156 +75,170 @@ export function EditEmployeeForm({ defaultValues }: { defaultValues: DefaultEmpl
   };
 
   return (
-    <section className="relative mx-auto grid grid-cols-1 md:grid-cols-5 gap-4 items-start">
-      <LoadingOverlay visible={updating.isLoading} />
-      <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4 col-span-3">
-        <Card withBorder shadow="md" p="lg" className="space-y-2 overflow-visible">
-          <h3 className="mb-3 text-xl flex items-baseline justify-between">
-            Profile
-            {isChanged && (
-              <div>
-                <Button type="button" className="mr-2" variant="default" compact onClick={cancelChanges}>
-                  Cancel
-                </Button>
-                <Button type="submit" compact>
-                  Save
-                </Button>
-              </div>
-            )}
-          </h3>
-          <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
-            <TextInput
-              label="Имя"
-              className="font-medium block"
-              type="text"
-              // required
-              {...register(
-                'firstName'
-                // { required: 'Необходимо заполнить' }
-              )}
-              error={errors.firstName?.message}
-            />
-            <TextInput
-              label="Фамилия"
-              className="font-medium block"
-              type="text"
-              // required
-              {...register(
-                'lastName'
-                // { required: 'Необходимо заполнить' }
-              )}
-              error={errors.lastName?.message}
-            />
-          </div>
-
-          <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
-            <TextInput
-              label="Эл. почта"
-              className="font-medium block"
-              type="email"
-              // required
-              {...register(
-                'email'
-                // { required: 'Необходимо заполнить' }
-              )}
-              error={errors.email?.message}
-            />
-            <TextInput
-              label="Номер телефона"
-              className="font-medium block"
-              type="tel"
-              {...register('phone')}
-              error={errors.phone?.message}
-            />
-          </div>
-
-          <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
-            <TextInput
-              label="Зарплата"
-              className="font-medium block"
-              type="number"
-              {...register('salary')}
-              error={errors.salary?.message}
-            />
-            <TextInput
-              label="Место жительства"
-              className="font-medium block"
-              type="text"
-              {...register('location')}
-              error={errors.location?.message}
-            />
-          </div>
-
-          <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
-            <Controller
-              control={control}
-              name="position"
-              // rules={{ required: 'Надо выбрать' }}
-              render={({ field: { value, onChange }, fieldState: { error } }) => {
-                return <SelectPosition setValue={setValue} value={value} onChange={onChange} error={error?.message} />;
-              }}
-            />
-            <Controller
-              control={control}
-              name="department"
-              // rules={{ required: 'Надо выбрать' }}
-              render={({ field: { value, onChange }, fieldState: { error } }) => {
-                return (
-                  <SelectDepartment setValue={setValue} value={value} onChange={onChange} error={error?.message} />
-                );
-              }}
-            />
-          </div>
-          <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
-            <div>
-              <Text size="sm" weight={500}>
-                Supervisor
-              </Text>
-              <SelectEmployee onChange={setSupervisor} defaultValue={supervisor?.id} />
-            </div>
-            <Controller
-              control={control}
-              name="status"
-              defaultValue={EmployeeStatus.NOT_ACCEPTED}
-              render={({ field: { value, onChange }, fieldState: { error } }) => (
+    <section className="mx-auto">
+      <Breadcrumbs className="mb-4">
+        {[
+          { title: 'Employees', href: '/employees' },
+          { title: `${defaultValues.form.firstName} ${defaultValues.form.lastName}`, href: `/employees/${id}` },
+        ].map((item) => (
+          <Anchor<typeof Link> component={Link} to={item.href} key={item.href}>
+            {item.title}
+          </Anchor>
+        ))}
+      </Breadcrumbs>
+      <div className="relative grid grid-cols-1 md:grid-cols-5 gap-4 items-start">
+        <LoadingOverlay visible={updating.isLoading} />
+        <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4 col-span-3">
+          <Card withBorder shadow="md" p="lg" className="space-y-2 overflow-visible">
+            <h3 className="mb-3 text-xl flex items-baseline justify-between">
+              Profile
+              {isChanged && (
                 <div>
-                  <Text size="sm" weight={500}>
-                    Status
-                  </Text>
-                  <SegmentedControl
-                    fullWidth
-                    value={value}
-                    color={
-                      value === EmployeeStatus.NOT_ACCEPTED ? 'gray' : value === EmployeeStatus.FIRED ? 'red' : 'teal'
-                    }
-                    onChange={onChange}
-                    data={[
-                      { label: 'Onboarding', value: EmployeeStatus.NOT_ACCEPTED },
-                      { label: 'Working', value: EmployeeStatus.WORKING },
-                      { label: 'Fired', value: EmployeeStatus.FIRED },
-                    ]}
-                  />
+                  <Button type="button" className="mr-2" variant="default" compact onClick={cancelChanges}>
+                    Cancel
+                  </Button>
+                  <Button type="submit" compact>
+                    Save
+                  </Button>
                 </div>
               )}
-            />
-          </div>
-        </Card>
+            </h3>
+            <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
+              <TextInput
+                label="Имя"
+                className="font-medium block"
+                type="text"
+                // required
+                {...register(
+                  'firstName'
+                  // { required: 'Необходимо заполнить' }
+                )}
+                error={errors.firstName?.message}
+              />
+              <TextInput
+                label="Фамилия"
+                className="font-medium block"
+                type="text"
+                // required
+                {...register(
+                  'lastName'
+                  // { required: 'Необходимо заполнить' }
+                )}
+                error={errors.lastName?.message}
+              />
+            </div>
 
-        <Card
-          withBorder
-          shadow="sm"
-          p="lg"
-          style={{
-            overflow: 'visible',
-          }}>
-          <div className="flex mb-3 items-center gap-x-4">
-            <h3 className="text-xl">Documents</h3>
-          </div>
-          <UploadFile uploaded={uploaded} setUploaded={setUploaded} />
-        </Card>
-      </form>
+            <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
+              <TextInput
+                label="Эл. почта"
+                className="font-medium block"
+                type="email"
+                // required
+                {...register(
+                  'email'
+                  // { required: 'Необходимо заполнить' }
+                )}
+                error={errors.email?.message}
+              />
+              <TextInput
+                label="Номер телефона"
+                className="font-medium block"
+                type="tel"
+                {...register('phone')}
+                error={errors.phone?.message}
+              />
+            </div>
 
-      <ApprovalsTimeline className="col-span-2" />
+            <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
+              <TextInput
+                label="Зарплата"
+                className="font-medium block"
+                type="number"
+                {...register('salary')}
+                error={errors.salary?.message}
+              />
+              <TextInput
+                label="Место жительства"
+                className="font-medium block"
+                type="text"
+                {...register('location')}
+                error={errors.location?.message}
+              />
+            </div>
+
+            <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
+              <Controller
+                control={control}
+                name="position"
+                // rules={{ required: 'Надо выбрать' }}
+                render={({ field: { value, onChange }, fieldState: { error } }) => {
+                  return (
+                    <SelectPosition setValue={setValue} value={value} onChange={onChange} error={error?.message} />
+                  );
+                }}
+              />
+              <Controller
+                control={control}
+                name="department"
+                // rules={{ required: 'Надо выбрать' }}
+                render={({ field: { value, onChange }, fieldState: { error } }) => {
+                  return (
+                    <SelectDepartment setValue={setValue} value={value} onChange={onChange} error={error?.message} />
+                  );
+                }}
+              />
+            </div>
+            <div className="grid gap-x-4 gap-y-2 sm:grid-cols-2">
+              <div>
+                <Text size="sm" weight={500}>
+                  Supervisor
+                </Text>
+                <SelectEmployee onChange={setSupervisor} defaultValue={supervisor?.id} />
+              </div>
+              <Controller
+                control={control}
+                name="status"
+                defaultValue={EmployeeStatus.NOT_ACCEPTED}
+                render={({ field: { value, onChange }, fieldState: { error } }) => (
+                  <div>
+                    <Text size="sm" weight={500}>
+                      Status
+                    </Text>
+                    <SegmentedControl
+                      fullWidth
+                      value={value}
+                      color={
+                        value === EmployeeStatus.NOT_ACCEPTED ? 'gray' : value === EmployeeStatus.FIRED ? 'red' : 'teal'
+                      }
+                      onChange={onChange}
+                      data={[
+                        { label: 'Onboarding', value: EmployeeStatus.NOT_ACCEPTED },
+                        { label: 'Working', value: EmployeeStatus.WORKING },
+                        { label: 'Fired', value: EmployeeStatus.FIRED },
+                      ]}
+                    />
+                  </div>
+                )}
+              />
+            </div>
+          </Card>
+
+          <Card
+            withBorder
+            shadow="sm"
+            p="lg"
+            style={{
+              overflow: 'visible',
+            }}>
+            <div className="flex mb-3 items-center gap-x-4">
+              <h3 className="text-xl">Documents</h3>
+            </div>
+            <UploadFile uploaded={uploaded} setUploaded={setUploaded} />
+          </Card>
+        </form>
+
+        <ApprovalsTimeline className="col-span-2" />
+      </div>
     </section>
   );
 }
