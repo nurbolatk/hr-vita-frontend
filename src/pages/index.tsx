9561b45fcde7159969e12b573b760b0a3ac3f9fa
nurbolatk <nurbolattk@gmail.com>
2022-04-15 23:22:09 +0600

    fix hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 export function LandingRoute() {
   return (
     <div>
-      <section className="grid grid-cols-2 pr-20">
-        <div className="flex flex-col justify-center items-start gap-y-6">
+      <section className="grid grid-cols-7 gap-x-4 ">
+        <div className="flex flex-col justify-center items-start gap-y-6 col-span-2">
           <h2 className="text-4xl">
             <span className="inline-block relative z-[1] before:bottom-0 before:left-0 before:right-0 before:-z-10 before:absolute before:bg-emerald-300 before:w-full before:h-1/3">
               Advanced
@@ -16,10 +16,10 @@ export function LandingRoute() {
           <h3>Chosen by the Kazakhstan&apos;s top companies</h3>
           <Button variant="filled">Get Started Free</Button>
         </div>
-        <div className="relative h-full py-6">
+        <div className="relative h-full py-6 col-start-4 col-span-4">
           <img src={heroImage} alt="hero" />
           <div
-            className="absolute animate-blob top-6 -left-16 w-64 h-64 bg-purple-300 rounded-full mix-blend-multiply opacity-70"
+            className="absolute animate-blob top-6 -left-12 w-64 h-64 bg-purple-300 rounded-full mix-blend-multiply opacity-70"
             style={{
               filter: 'blur(1.5rem)',
             }}
